6f4101e7b05485ccef0592f656e4efa7b23f6b1c
Mikhail Apsalikov <dee_jay@bk.ru>
2017-05-07 15:06:51 +0400

    [https://github.com/MikhailApsalikov/Veronika/issues/24] Details page is implemented

---
+++ Code/Beskova.Ontology/Beskova.Ontology.Web/AngularApp/Controllers/scientificSpecialityController.js
@@ -0,0 +1,39 @@
+﻿(function () {
+	'use strict';
+	var angular = window.angular;
+	angular
+		.module('APP')
+		.controller('scientificSpecialityController', ['$scope', 'loginService', 'baseService', 'messageService', scientificSpecialityController]);
+
+	function scientificSpecialityController($scope, loginService, service) {
+		$scope.data = {};
+		$scope.isLoaded = false;
+
+		$scope.reload = function () {
+			$scope.isLoaded = false;
+			service.getById("scientificSpeciality", getId()).then(function (data) {
+				$scope.data = data;
+				$scope.isLoaded = true;
+			});
+		};
+
+		$scope.specDetails = function (id) {
+			window.location.href = "/Speciality#id=" + id;
+		};
+
+		function getId() {
+			try {
+				return /\#id=(.*)/.exec(window.location.hash)[1];
+			}
+			catch (e) {
+				window.location.href = window.baseUri;
+			}
+		}
+
+		function activate() {
+			$scope.reload();
+		};
+
+		activate();
+	}
+})();
